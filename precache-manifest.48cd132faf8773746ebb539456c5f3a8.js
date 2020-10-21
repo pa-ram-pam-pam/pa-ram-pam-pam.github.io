4839a2850a8d47c6352e3e65dd84ddc261d209a6
Baltasev Sergey <64358645+pa-ram-pam-pam@users.noreply.github.com>
2020-10-21 19:37:14 +0500

    Add files via upload

---
+++ precache-manifest.48cd132faf8773746ebb539456c5f3a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2579de5c7c0bc9e1a593828207a2b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a19ef96117c257158ed8",
+    "url": "/static/js/2.eee1f9a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eee1f9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3259f5bd1b62a1de140c",
+    "url": "/static/js/main.d912ca51.chunk.js"
+  },
+  {
+    "revision": "345ee3624a29d86da6e6",
+    "url": "/static/js/runtime-main.cac535e6.js"
+  }
+]);
